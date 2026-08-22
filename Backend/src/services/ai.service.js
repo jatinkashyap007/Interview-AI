a004255ca0c714b16e4c2db3baa0711716eea66b
Jatin Kashyap <jkashyap891@gmail.com>
2026-08-23 04:43:37 +0530

    Set Gemini model to gemini-3-flash-preview

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -42,7 +42,7 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 `
 
     const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-3-flash-preview",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
@@ -99,7 +99,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-3-flash-preview",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
